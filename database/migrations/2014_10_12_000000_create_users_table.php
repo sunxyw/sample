3c64e0088d30e1792fe841661a7c276b17367dc8
sunxyw <xy2496419818@gmail.com>
2018-03-22 19:20:39 +0800

    Move user model to models folder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,10 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->increments('id', 11);
+            $table->string('name', 60);
+            $table->string('email', 60)->unique();
+            $table->string('password', 60);
             $table->rememberToken();
             $table->timestamps();
         });
